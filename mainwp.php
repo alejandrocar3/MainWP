e8c8f73c2c4fec1af9d94409be32ac4693985944
Alejandro Carlos <webdev.carlos569@gmail.com>
2025-07-22 23:06:17 +0600

    mainwp.php

--- mainwp.php
+++ mainwp.php
@@ -3,7 +3,7 @@
 /*
   Plugin Name: MainWP Dashboard
   Plugin URI: https://mainwp.com/
-  Description: Manage all of your WP sites, even those on different servers, from one central dashboard that runs off of your own self-hosted WordPress install..
+  Description: Manage all of your WP sites, even those on different servers, from one central dashboard that runs off of your own self-hosted WordPress install.
   Author: MainWP
   Author URI: https://mainwp.com
   Text Domain: mainwp
